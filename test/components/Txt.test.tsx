9abb613d3791902b6be6fe3bf03df9166ad82fde
First-Terraner <m.echerbebo@gmail.com>
2023-06-08 22:46:45 +0200

    test the Txt component

--- test/components/Txt.test.tsx
+++ test/components/Txt.test.tsx
@@ -7,4 +7,19 @@ describe('Basic test of the Txt.tsx component', () => {
 		const textElement = getByText('Hello World!')
 		expect(textElement).toBeDefined()
 	})
+	it('has the global styles of a text component', () => {
+		const { getByText } = render(<Txt txt='Hello World!' />)
+		const textElement = getByText('Hello World!')
+		expect(textElement.props.style[0]).toStrictEqual({
+			fontSize: 16,
+			color: ''
+		})
+	})
+	it('has an additional specific style applied', () => {
+		const { getByText } = render(
+			<Txt txt='Hello World!' styles={[{ marginTop: 10 }]} />
+		)
+		const textElement = getByText('Hello World!')
+		expect(textElement.props.style[1].marginTop).toBe(10)
+	})
 })
