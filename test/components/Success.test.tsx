875f42fedd05cc12f3b9f6d8af71de2937cdc1f3
KKA11010 <m.echerbebo@gmail.com>
2023-06-10 01:37:59 +0200

    add success page test

--- test/components/Success.test.tsx
+++ test/components/Success.test.tsx
@@ -1,5 +1,6 @@
 import Success from '@comps/Success'
-import { render, screen } from '@testing-library/react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { fireEvent, render, screen } from '@testing-library/react-native'
 // {
 // 	amount: number
 // 	fee?: number
@@ -9,43 +10,75 @@ import { render, screen } from '@testing-library/react-native'
 // 	nav?: NativeStackNavigationProp<RootStackParamList, 'success', 'MyStack'>
 // 	hash?: string
 // }
+// Mock the navigation object
+const mockNavigation = {
+	navigate: jest.fn(),
+}
+
+// Mock the NavigationContainer
+jest.mock('@react-navigation/native', () => ({
+	...jest.requireActual('@react-navigation/native'),
+	useNavigation: () => mockNavigation,
+}))
+
 describe('Basic test of the Success.tsx component', () => {
 	it('renders the success screen for payment from the mint to a LN wallet', () => {
 		render(
-			<Success
-				amount={21}
-				fee={0}
-				mints={['test-mint']}
-			/>
+			<NavigationContainer>
+				<Success
+					amount={21}
+					fee={0}
+					mints={['test-mint']}
+				/>
+			</NavigationContainer>
 		)
 		const textElement = screen.getByText('Payment successfull!')
 		expect(textElement).toBeDefined()
 	})
 	it('renders the success screen for minting new tokens', () => {
 		render(
-			<Success
-				amount={21}
-				hash='test-hash'
-				mint='test-mint'
-			/>
+			<NavigationContainer>
+				<Success
+					amount={21}
+					hash='test-hash'
+					mint='test-mint'
+				/>
+			</NavigationContainer>
 		)
 		const textElement = screen.getByText('21 Satoshi minted!')
 		expect(textElement).toBeDefined()
 	})
-
-	// it('has the global styles of a text component', () => {
-	// 	const { getByText } = render(<Txt txt='Hello World!' />)
-	// 	const textElement = getByText('Hello World!')
-	// 	expect(textElement.props.style[0]).toStrictEqual({
-	// 		fontSize: 16,
-	// 		color: '#656565'
-	// 	})
-	// })
-	// it('has an additional specific style applied', () => {
-	// 	const { getByText } = render(
-	// 		<Txt txt='Hello World!' styles={[{ marginTop: 10 }]} />
+	it('renders the success screen after claiming a new token', () => {
+		render(
+			<NavigationContainer>
+				<Success
+					amount={21}
+					mints={['test-mint']}
+					memo='Just a test'
+				/>
+			</NavigationContainer>
+		)
+		const textElement = screen.getByText('21 Satoshi claimed!')
+		expect(textElement).toBeDefined()
+		const memo = screen.getByText('Just a test')
+		expect(memo).toBeDefined()
+	})
+	// it('navigates to a specific screen', () => {
+	// 	render(
+	// 		<NavigationContainer>
+	// 			<Success
+	// 				amount={21}
+	// 				mints={['test-mint']}
+	// 				memo='Just a test'
+	// 			/>
+	// 		</NavigationContainer>
 	// 	)
-	// 	const textElement = getByText('Hello World!')
-	// 	expect(textElement.props.style[1].marginTop).toBe(10)
+	// 	const pressElement = screen.getByText('Back to dashboard')
+	// 	fireEvent.press(pressElement)
+	// 	const expectedComponent = screen.getByTestId('dashboard')
+	// 	// Verify if the expected component is rendered
+	// 	expect(expectedComponent).toBeDefined()
+	// 	// Verify if the navigate function was called with the correct screen name
+	// 	expect(mockNavigation.navigate).toHaveBeenCalledWith('dashboard')
 	// })
 })
