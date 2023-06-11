6e7b26907a553272e4aecd8f1f021c6bba339a11
BilligsterUser <billigsteruser@protonmail.com>
2023-06-11 14:01:58 +0200

    add config

---
+++ src/config/config.base.ts
@@ -0,0 +1,30 @@
+import { env } from '@src/consts'
+
+export interface ConfigBaseProps {
+	persistNavigation: 'always' | 'dev' | 'prod' | 'never'
+	catchErrors: 'always' | 'dev' | 'prod' | 'never'
+	exitRoutes: string[]
+	env: typeof env
+}
+
+export type PersistNavigationConfig = ConfigBaseProps['persistNavigation']
+
+const BaseConfig = {
+	// This feature is particularly useful in development mode, but
+	// can be used in production as well if you prefer.
+	persistNavigation: 'dev',
+
+	/**
+   * Only enable if we're catching errors in the right environment
+   */
+	catchErrors: 'always',
+
+	/**
+   * This is a list of all the route names that will exit the app if the back button
+   * is pressed while in that screen. Only affects Android.
+   */
+	exitRoutes: ['Welcome'],
+	env
+} as const
+
+export default BaseConfig
