c33417df4a8068916ea8d9f278154ff2c4adfe42
First-Terraner <m.echerbebo@gmail.com>
2023-07-24 01:30:48 +0200

    update disclaimer screen

--- src/components/screens/Disclaimer.tsx
+++ src/components/screens/Disclaimer.tsx
@@ -1,21 +1,39 @@
 import Button from '@comps/Button'
+import Separator from '@comps/Separator'
 import Txt from '@comps/Txt'
 import type { TDisclaimerPageProps } from '@model/nav'
 import TopNav from '@nav/TopNav'
 import { ThemeContext } from '@src/context/Theme'
+import { globals } from '@styles'
 import { useContext } from 'react'
 import { useTranslation } from 'react-i18next'
-import { StyleSheet, View } from 'react-native'
+import { ScrollView, StyleSheet, View } from 'react-native'
 
 export function Disclaimer({ navigation }: TDisclaimerPageProps) {
 	const { color } = useContext(ThemeContext)
-	const { t } = useTranslation(['common'])
+	const { t } = useTranslation(['wallet'])
 	return (
 		<View style={{ flex: 1, backgroundColor: color.BACKGROUND }}>
-			<TopNav screenName={t('disclaimer')} withBackBtn />
-			<View style={styles.container}>
-				<Txt txt={t('disclaimer', { ns: 'wallet' })} />
-			</View>
+			<TopNav screenName={t('disclaimer', { ns: 'common' })} withBackBtn />
+			<ScrollView style={styles.container}>
+				<View style={[globals(color).wrapContainer, { paddingVertical: 20 }]}>
+					{/* beta */}
+					<Txt txt={t('enutsDisclaimer')} styles={[styles.header]} />
+					<Txt txt={t('disclaimer')} />
+					<Separator style={[styles.separator]} />
+					{/* custodial */}
+					<Txt txt={t('custodialRisk')} styles={[styles.header]} />
+					<Txt txt={t('custodialRiskContent')} />
+					<Separator style={[styles.separator]} />
+					{/* cashu */}
+					<Txt txt={t('cashuExperiment')} styles={[styles.header]} />
+					<Txt txt={t('cashuContent')} />
+					<Separator style={[styles.separator]} />
+					{/* loss */}
+					<Txt txt={t('lossOfTokens')} styles={[styles.header]} />
+					<Txt txt={t('lossContent')} />
+				</View>
+			</ScrollView>
 			<View style={styles.ok}>
 				<Button
 					txt='OK'
@@ -28,13 +46,21 @@ export function Disclaimer({ navigation }: TDisclaimerPageProps) {
 
 const styles = StyleSheet.create({
 	container: {
-		marginTop: 110,
-		paddingHorizontal: 20,
+		marginTop: 100,
+		marginBottom: 110,
+	},
+	header: {
+		fontWeight: '500',
+		fontSize: 18,
+		marginBottom: 10
+	},
+	separator: {
+		marginVertical: 20
 	},
 	ok: {
 		position: 'absolute',
 		bottom: 20,
 		right: 20,
 		left: 20,
-	}
+	},
 })
