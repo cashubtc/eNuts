4fa5eef5e394043200f1d0c5dc6eab17831b90b7
KKA11010 <m.echerbebo@gmail.com>
2023-09-18 19:47:18 +0200

    hide wallet amounts by pressing on logo #226

--- src/screens/Settings/Privacy.tsx
+++ src/screens/Settings/Privacy.tsx
@@ -7,8 +7,6 @@ import BottomNav from '@nav/BottomNav'
 import { usePrivacyContext } from '@src/context/Privacy'
 import { useThemeContext } from '@src/context/Theme'
 import { NS } from '@src/i18n'
-import { store } from '@store'
-import { STORE_KEYS } from '@store/consts'
 import { globals, highlight as hi } from '@styles'
 import { useTranslation } from 'react-i18next'
 import { StyleSheet, Switch, Text, View } from 'react-native'
@@ -16,25 +14,7 @@ import { StyleSheet, Switch, Text, View } from 'react-native'
 export default function PrivacySettings({ navigation, route }: TPrivacySettingsPageProps) {
 	const { t } = useTranslation([NS.topNav])
 	const { color, highlight } = useThemeContext()
-	const { hidden, setHidden } = usePrivacyContext()
-
-	const handleHiddenBalance = async () => {
-		setHidden({ balance: !hidden.balance, txs: hidden.txs })
-		if (hidden.balance) {
-			await store.delete(STORE_KEYS.hiddenBal)
-			return
-		}
-		await store.set(STORE_KEYS.hiddenBal, '1')
-	}
-
-	const handleHiddenTxs = async () => {
-		setHidden({ balance: hidden.balance, txs: !hidden.txs })
-		if (hidden.txs) {
-			await store.delete(STORE_KEYS.hiddenTxs)
-			return
-		}
-		await store.set(STORE_KEYS.hiddenTxs, '1')
-	}
+	const { hidden, handleHiddenBalance, handleHiddenTxs } = usePrivacyContext()
 
 	return (
 		<Screen
