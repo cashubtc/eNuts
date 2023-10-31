a013cda0b7c27afacf2b8e7f38bcbc51c65de53c
First-Terraner <m.echerbebo@gmail.com>
2023-10-31 22:44:55 +0100

    comment out async-storage mock

--- test/setup.ts
+++ test/setup.ts
@@ -43,9 +43,9 @@ jest.mock('reactotron-react-native', () => ({}))
 // jest.mock('@consts', () => ({}))
 jest.mock('expo/config', () => ({}))
 
-jest.doMock('@react-native-async-storage/async-storage', () =>
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-return
-	require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
-)
+// jest.doMock('@react-native-async-storage/async-storage', () =>
+// 	// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+// 	require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
+// )
 
 declare global { const __TEST__ = true }
