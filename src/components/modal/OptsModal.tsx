6480502f1f5f80881af68b65aeda1834a054fe66
KKA11010 <m.echerbebo@gmail.com>
2023-12-19 13:58:05 +0100

    update options modal

--- src/components/modal/OptsModal.tsx
+++ src/components/modal/OptsModal.tsx
@@ -1,13 +1,14 @@
 import { TxtButton } from '@comps/Button'
-import { CopyIcon, NostrIcon, ReceiveIcon, SendMsgIcon, ZapIcon } from '@comps/Icons'
-import Option from '@comps/Option'
+import { CopyIcon, NostrIcon, SendMsgIcon, ZapIcon } from '@comps/Icons'
+import Loading from '@comps/Loading'
+import Separator from '@comps/Separator'
 import Txt from '@comps/Txt'
 import { useNostrContext } from '@src/context/Nostr'
 import { useThemeContext } from '@src/context/Theme'
 import { NS } from '@src/i18n'
 import { mainColors } from '@styles'
 import { useTranslation } from 'react-i18next'
-import { ScrollView } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
 import { s, ScaledSheet, vs } from 'react-native-size-matters'
 
 import MyModal from '.'
@@ -46,48 +47,106 @@ export default function OptsModal({
 				center
 				styles={[styles.hint]}
 			/>
-			<ScrollView style={styles.optionWrap} alwaysBounceVertical={false}>
-				<Option
-					icon={isSend ? <SendMsgIcon width={s(16)} height={s(16)} color={mainColors.VALID} /> : <CopyIcon color={mainColors.VALID} />}
-					txt={button1Txt}
-					hint={isSend ? t('sendEcashDashboard') : t('receiveEcashDashboard')}
-					onPress={onPressFirstBtn}
-					hasSeparator
-					loading={loading}
-					secondIcon={!isSend && <ReceiveIcon width={s(26)} height={s(26)} color={color.TEXT} />}
-				/>
-				{!isSend && nutPub.length > 0 &&
-					<Option
-						icon={<NostrIcon />}
-						txt={t('receiveEcashNostr')}
-						hint={t('receiveEcashNostrHint')}
+			<TouchableOpacity
+				style={styles.container}
+				onPress={onPressFirstBtn}
+			>
+				<View style={styles.iconContainer}>
+					{isSend ?
+						<SendMsgIcon width={s(16)} height={s(16)} color={mainColors.VALID} />
+						:
+						loading ?
+							<View>
+								<Loading size='small' color={mainColors.VALID} />
+							</View>
+							:
+							<CopyIcon color={mainColors.VALID} />
+					}
+				</View>
+				<View style={styles.txtWrap}>
+					<Text style={[styles.actionText, { color: color.TEXT }]}>
+						{button1Txt}
+					</Text>
+					<Text style={[styles.descriptionText, { color: color.TEXT_SECONDARY }]}>
+						{isSend ? t('sendEcashDashboard') : t('receiveEcashDashboard')}
+					</Text>
+				</View>
+			</TouchableOpacity>
+			<Separator style={[styles.separator]} />
+			{!isSend && nutPub.length > 0 &&
+				<>
+					<TouchableOpacity
+						style={styles.container}
 						onPress={() => handleNostrReceive?.()}
-						hasSeparator
-					/>
-				}
-				<Option
-					icon={<ZapIcon width={s(26)} height={s(26)} color={mainColors.ZAP} />}
-					txt={button2Txt}
-					hint={isSend ? t('payInvoiceDashboard') : t('createInvoiceDashboard')}
-					onPress={onPressSecondBtn}
-				/>
-				<TxtButton
-					txt={t('cancel')}
-					onPress={onPressCancel}
-					style={[{ paddingBottom: vs(15), paddingTop: vs(15) }]}
-				/>
-			</ScrollView>
+					>
+						<View style={styles.iconContainer}>
+							<NostrIcon />
+						</View>
+						<View style={styles.txtWrap}>
+							<Text style={[styles.actionText, { color: color.TEXT }]}>
+								{t('receiveEcashNostr')}
+							</Text>
+							<Text style={[styles.descriptionText, { color: color.TEXT_SECONDARY }]}>
+								{t('receiveEcashNostrHint')}
+							</Text>
+						</View>
+					</TouchableOpacity>
+					<Separator style={[styles.separator]} />
+				</>
+			}
+			<TouchableOpacity
+				style={styles.container}
+				onPress={onPressSecondBtn}
+			>
+				<View style={styles.iconContainer}>
+					<ZapIcon width={s(26)} height={s(26)} color={mainColors.ZAP} />
+				</View>
+				<View style={styles.txtWrap}>
+					<Text style={[styles.actionText, { color: color.TEXT }]}>
+						{button2Txt}
+					</Text>
+					<Text style={[styles.descriptionText, { color: color.TEXT_SECONDARY }]}>
+						{isSend ? t('payInvoiceDashboard') : t('createInvoiceDashboard')}
+					</Text>
+				</View>
+			</TouchableOpacity>
+			<TxtButton
+				txt={t('cancel')}
+				onPress={onPressCancel}
+				style={[{ paddingBottom: vs(15), paddingTop: vs(25) }]}
+			/>
 		</MyModal>
 	)
 }
 
 const styles = ScaledSheet.create({
-	optionWrap: {
+	container: {
+		flexDirection: 'row',
+		alignItems: 'center',
 		width: '100%',
-		paddingHorizontal: '10@s'
+	},
+	iconContainer: {
+		minWidth: '10%',
+	},
+	txtWrap: {
+		width: '90%',
+	},
+	actionText: {
+		fontSize: '14@vs',
+		fontWeight: '500',
+		marginBottom: '4@vs',
+	},
+	descriptionText: {
+		fontSize: '12@vs',
 	},
 	hint: {
 		fontSize: '18@vs',
+		marginTop: '5@vs',
 		marginBottom: '30@vs',
 	},
+	separator: {
+		width: '100%',
+		marginTop: '10@vs',
+		marginBottom: '10@vs'
+	}
 })
