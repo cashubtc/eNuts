779ca367bcf806af2d69e4d647c8bc2c6c568035
First-Terraner <m.echerbebo@gmail.com>
2023-12-26 21:55:19 +0100

    add use-count-up hook lib, animate balance

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -11,10 +11,11 @@ import { NS } from '@src/i18n'
 import { globals, highlight as hi } from '@styles'
 import { getColor } from '@styles/colors'
 import { formatBalance, formatInt, formatSatStr, isBool } from '@util'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Text, TouchableOpacity, View } from 'react-native'
 import { s, ScaledSheet, vs } from 'react-native-size-matters'
+import { CountUp } from 'use-count-up'
 
 import { TxtButton } from './Button'
 import Logo from './Logo'
@@ -31,6 +32,8 @@ export default function Balance({ nav }: IBalanceProps) {
 	const { hidden, handleLogoPress } = usePrivacyContext()
 	const [formatSats, setFormatSats] = useState(pref?.formatBalance)
 	const { latestHistory } = useHistoryContext()
+	const oldBal = useRef(balance)
+	const diff = useRef(Math.abs(balance - oldBal.current))
 
 	const toggleBalanceFormat = () => {
 		setFormatSats(prev => !prev)
@@ -45,14 +48,17 @@ export default function Balance({ nav }: IBalanceProps) {
 		return t('swap')
 	}
 
+	useEffect(() => {
+		if (oldBal.current === balance) { return }
+		diff.current = Math.abs(balance - oldBal.current)
+	}, [balance])
+
 	return (
 		<View style={[
 			styles.board,
 			{ borderColor: color.BORDER, backgroundColor: hi[highlight] }
 		]}>
-			<TouchableOpacity
-				onPress={() => void handleLogoPress()}
-			>
+			<TouchableOpacity onPress={() => void handleLogoPress()}>
 				<Logo size={hidden.balance ? s(100) : s(40)} style={{ marginTop: hidden.balance ? vs(40) : 0, marginBottom: hidden.balance ? vs(40) : vs(10) }} />
 			</TouchableOpacity>
 			{/* balance */}
@@ -63,7 +69,16 @@ export default function Balance({ nav }: IBalanceProps) {
 					disabled={hidden.balance}
 				>
 					<Text style={[styles.balAmount, { color: getColor(highlight, color) }]}>
-						{formatSats ? formatBalance(balance) : formatInt(balance)}
+						<CountUp
+							key={balance}
+							start={oldBal.current}
+							end={balance}
+							duration={1.5}
+							isCounting
+							decimalPlaces={0}
+							formatter={value => formatSats ? formatBalance(Math.trunc(value)) : formatInt(Math.trunc(value))}
+							onComplete={() => { oldBal.current = balance }}
+						/>
 					</Text>
 					<View style={styles.balAssetNameWrap}>
 						<Text style={[styles.balAssetName, { color: getColor(highlight, color) }]}>
