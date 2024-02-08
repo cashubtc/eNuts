02691e5ea45cdb772229d1879e159f1afebdd414
First-Terraner <m.echerbebo@gmail.com>
2024-02-08 10:29:05 +0100

    remove debuging logs

--- src/storage/store/restore.ts
+++ src/storage/store/restore.ts
@@ -7,7 +7,6 @@ import { SecureStore } from './SecureStore'
 export async function saveSeed(seed: Uint8Array) {
 	try {
 		const str = Buffer.from(seed).toString('base64')
-		l('[saveSeed] base64 ', { seed, base64: str })
 		await SecureStore.set(STORE_KEYS.seed, str)
 	} catch (e) {
 		l('[saveSeed] error', { e })
@@ -19,9 +18,7 @@ export async function getSeed() {
 	try {
 		const str = await SecureStore.get(STORE_KEYS.seed)
 		const buffer = str ? Buffer.from(str, 'base64') : undefined
-		const seed = buffer ? new Uint8Array(buffer) : undefined
-		l('[getSeed] base64 ', { stored: str, seed })
-		return seed
+		return buffer ? new Uint8Array(buffer) : undefined
 	} catch (e) {
 		l('[getSeed] error', { e })
 		throw new Error('[getSeed] error')
