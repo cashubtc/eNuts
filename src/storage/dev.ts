69e103bd0fd845f2c0606b69cb1a016abff47ad0
Egge <egge@getcurrent.io>
2025-07-17 09:39:07 +0200

    refactor: Remove TTLCache and related references from storage

--- src/storage/dev.ts
+++ src/storage/dev.ts
@@ -3,7 +3,6 @@ import * as FileSystem from "expo-file-system";
 import { dropAll } from "./db";
 import { historyStore, secureStore, store } from "./store";
 import { SECRET, SECURESTORE_KEY, STORE_KEYS } from "./store/consts";
-import { ttlCache } from "./store/ttl";
 
 export async function dropAllData() {
     await Promise.all([
@@ -13,7 +12,6 @@ export async function dropAllData() {
         secureStore.delete(SECURESTORE_KEY),
         secureStore.delete(STORE_KEYS.seed),
         historyStore.clear(),
-        ttlCache.clear(),
         FileSystem.deleteLegacyDocumentDirectoryAndroid(),
         FileSystem.deleteAsync(FileSystem.cacheDirectory!),
         FileSystem.deleteAsync(FileSystem.documentDirectory!),
