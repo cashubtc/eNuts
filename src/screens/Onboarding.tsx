063b5f1d4e30b9fe31479b7e531f3e9f1db5baf6
Egge <egge@my2sats.space>
2025-10-30 13:51:48 +0100

    feat: added alpha warning to onboarding

--- src/screens/Onboarding.tsx
+++ src/screens/Onboarding.tsx
@@ -1,17 +1,20 @@
 import Logo from "@comps/Logo";
+import RadioBtn from "@comps/RadioBtn";
 import Txt from "@comps/Txt";
 import type { TOnboardingPageProps } from "@model/nav";
 import { NS } from "@src/i18n";
 import { store } from "@src/storage/store";
 import { STORE_KEYS } from "@src/storage/store/consts";
 import { H_Colors, mainColors } from "@styles/colors";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Image, TouchableOpacity } from "react-native";
+import { Image, TouchableOpacity, View } from "react-native";
 import Onboarding from "react-native-onboarding-swiper";
 import { s, ScaledSheet } from "react-native-size-matters";
 
 export default function OnboardingScreen({ navigation }: TOnboardingPageProps) {
   const { t } = useTranslation([NS.common]);
+  const [accepted, setAccepted] = useState(false);
   const handleDone = async () => {
     await store.set(STORE_KEYS.explainer, "1");
     navigation.replace("dashboard");
@@ -19,7 +22,50 @@ export default function OnboardingScreen({ navigation }: TOnboardingPageProps) {
   return (
     <Onboarding
       onDone={() => void handleDone()}
+      showDone={accepted}
+      showSkip={accepted}
+      showNext={accepted}
       pages={[
+        {
+          backgroundColor: "black",
+          image: <Logo size={s(130)} />,
+          title: "Alpha Testing",
+          subtitle: (
+            <View
+              style={{
+                padding: s(10),
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Txt
+                txt="eNuts is currently in alpha testing. Please use at your own risk."
+                styles={[{ paddingHorizontal: s(10), textAlign: "center" }]}
+              />
+              <TouchableOpacity
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  marginTop: s(24),
+                }}
+                onPress={() => setAccepted((acpt) => !acpt)}
+                activeOpacity={0.7}
+                testID="onboarding-accept-checkbox"
+              >
+                <View
+                  style={{
+                    width: s(10),
+                    borderWidth: 1,
+                    borderColor: "white",
+                    height: s(10),
+                    backgroundColor: accepted ? "white" : "transparent",
+                  }}
+                />
+                <Txt txt="I understand" styles={[{ marginLeft: s(10) }]} />
+              </TouchableOpacity>
+            </View>
+          ),
+        },
         {
           backgroundColor: H_Colors.Default,
           image: <Logo size={s(130)} />,
